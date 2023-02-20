1579471940c54a70fd0dd45ca8088d88a853768f
chrislaynexavier <chrislayne_pereira@hotmail.com>
2023-02-20 11:10:49 -0300

    add new groups to user

--- node/src/domain/entity/User.ts
+++ node/src/domain/entity/User.ts
@@ -1,9 +1,12 @@
 import { DailyHabit } from "./DailyHabit"
+import { Group } from "./Group"
 
 export class User {
     public email!: string
     public password!: string
     public dailiesHabits: DailyHabit[] = []
+    public groups: Group[] = []
+    
     constructor(email: string, password: string){
         this.email = email
         this.password = password
--- node/test/User.spec.ts
+++ node/test/User.spec.ts
@@ -1,16 +1,20 @@
 import { DailyHabit } from "../src/domain/entity/DailyHabit"
 import { User } from "../src/domain/entity/User"
+import { Group } from "../src/domain/entity/Group"
 
 describe('User', () => {
     let newUser: User
     let email = "chris@gmail.com"
     let password = "123456"
-    let dailyHabit!: DailyHabit
+    let dailyHabit: DailyHabit
     let date = new Date()
+    let group: Group
+    let nameGroup = "musas fitness"
 
     beforeEach(() => {
       newUser = new User(email, password)
       dailyHabit = new DailyHabit(date)
+      group = new Group(nameGroup)
     })
 
     it('should have a user with correct e-mail', () => {
@@ -29,5 +33,10 @@ describe('User', () => {
     it('should daily habit has a user', () => {
       newUser.addDailyHabit(dailyHabit)
       expect(dailyHabit.user).toBe(newUser)
+    })
+
+    it('should contain the new group added in User', () => {
+      group.addUser(newUser)
+      expect(newUser.groups).toContain(group)
     })
 })
